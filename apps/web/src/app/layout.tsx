176e614f258546842d23bb8ae86d0b29f27a028f
decker <deckerurbano@gmail.com>
2024-12-28 00:15:59 -0300

    Implement web application structure with Next.js and Tailwind CSS. Added essential configuration files including package.json, postcss.config.mjs, and tsconfig.json. Introduced layout and page components, along with favicon and logo assets. Cleared environment variables in turbo.json for a cleaner build process.

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,10 @@
+import "@dkr/ui/globals.css";
+import type { ReactNode } from "react";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>) {
+  return { children };
+}
